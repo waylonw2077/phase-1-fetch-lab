7a48b1d04a29ac46fd2fa699834763addc3d2a7c
waylon wickline <waylonw2077@LAPTOP-6MQVH55Q>
2023-03-27 15:24:04 -0400

    almost completes lab

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(json => renderBooks(json));
 }
 
 function renderBooks(books) {
